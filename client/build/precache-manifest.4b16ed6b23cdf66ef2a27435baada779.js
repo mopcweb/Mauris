9a3c204d243ef7751b0e2fe09069ee087974b5e8
Danil Moroz <danilmoroz1995@gmail.com>
2018-12-21 19:44:23 +0200

    forgot to add build

---
+++ client/build/precache-manifest.4b16ed6b23cdf66ef2a27435baada779.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a78869657a9e501a0a7a2e2fdf5357a0",
+    "url": "/static/media/promo.a7886965.png"
+  },
+  {
+    "revision": "6a9f050b4b0cb537f09fe1f96a733833",
+    "url": "/static/media/bg.6a9f050b.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "512faa8aabcf90e642e9",
+    "url": "/static/js/main.512faa8a.chunk.js"
+  },
+  {
+    "revision": "1830595a03b7624c8a11",
+    "url": "/static/js/1.1830595a.chunk.js"
+  },
+  {
+    "revision": "512faa8aabcf90e642e9",
+    "url": "/static/css/main.08a6ee66.chunk.css"
+  },
+  {
+    "revision": "48397e4c32a6693ce21bc241da63c966",
+    "url": "/index.html"
+  }
+];
